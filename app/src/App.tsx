547a2fcc689c2b98a1f83de9ea1405953bced1d8
musteve <mu23818@g.ecc.u-tokyo.ac.jp>
2023-10-23 18:44:03 +0900

    new

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,23 +1,61 @@
-import { Route, Routes } from 'react-router-dom';
-import Books from './books';
-import Home from './home';
-import Header from './header';
-import Demo from './demo';
+'use client';
+import { useDisclosure } from '@mantine/hooks';
+import { AppShell, Burger, Group, Stack, Text } from '@mantine/core';
+import { IconCalendarEvent } from '@tabler/icons-react';
 
-
-function App() {
-    return (
-        <>
-            <Header />
-
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path='/books' element={<Books />} />
-                <Route path="/demo" element={<Demo />} />
-            </Routes>
-
-        </>
-    );
+export function BottomNavigation(): JSX.Element {
+  return (
+    <AppShell.Footer>
+      <Group grow gap={0}>
+        <Stack h={60} align='center' justify='center' gap={0}>
+          <IconCalendarEvent />
+          <Text
+            size='xs'
+          >
+            Events
+          </Text>
+        </Stack>
+        <Stack h={60} align='center' justify='center' gap={0}>
+          <IconCalendarEvent />
+          <Text
+            size='xs'
+          >
+            Events
+          </Text>
+        </Stack>
+        <Stack h={60} align='center' justify='center' gap={0}>
+          <IconCalendarEvent />
+          <Text
+            size='xs'
+          >
+            Events
+          </Text>
+        </Stack>
+      </Group>
+    </AppShell.Footer>
+  );
 }
 
-export default App;
+export default function HomePage() {
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+      footer={{ height: 60 }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        <div>Logo</div>
+      </AppShell.Header>
+
+      <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+
+      <AppShell.Main>Main</AppShell.Main>
+
+      <BottomNavigation />
+    </AppShell>
+  );
+}
