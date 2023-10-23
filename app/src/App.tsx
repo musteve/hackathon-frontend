83ed31674a9810877fc809075dc730559ac29b2c
musteve <mu23818@g.ecc.u-tokyo.ac.jp>
2023-10-23 15:59:59 +0900

    new

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,21 +1,27 @@
 import { Route, Routes } from 'react-router-dom';
 import Books from './books';
 import Home from './home';
+import { AppShell, Group } from '@mantine/core';
 
 
 function App() {
     return (
-       <div>
-            <header>
-                <h1>header header header</h1>
-            </header>
-
+       <AppShell
+            padding="md"
+            header={{height: 60}}
+       >
+            <AppShell.Header>
+                <Group h="100%" px="md">
+                    <text>HEADER</text>
+                </Group>
+            </AppShell.Header>
+            
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path='/books' element={<Books />} />
             </Routes>
 
-       </div>
+       </AppShell>
     );
 }
 
