8e864db98ce884a17ff1e5e5937d8831258aeb6d
musteve <mu23818@g.ecc.u-tokyo.ac.jp>
2023-11-04 16:14:10 +0900

    update

--- app/src/http/demo/get-personal-data.tsx
+++ app/src/http/demo/get-personal-data.tsx
@@ -8,7 +8,7 @@ function GetPersonalData() {
         const fetchData = async () => {
             try {
                 const response = await fetch (
-                    "https://hackathon-backend-main-mxtuefqkua-uc.a.run.app/getusers",
+                    "https://hackathon-backend-main-mxtuefqkua-uc.a.run.app/demo",
                     {
                         method: "GET",
                         headers: {
